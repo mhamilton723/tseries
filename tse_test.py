36157708942501709be5b74f71058e106184ed34
mhamilton723 <mark.hamilton@yale.edu>
2015-12-15 15:19:48 -0500

    Fixed Figure in Demo and expanded Demo
    Fixed Figure in Demo and Expanded Demo

--- tse_test.py
+++ tse_test.py
@@ -4,66 +4,35 @@ from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
-from utils import datasets, safe_shape, mse
+from utils import datasets, safe_shape
 from TimeSeriesEstimator import TimeSeriesRegressor, time_series_split, time_series_cv, cascade_cv
 from sklearn.cross_validation import KFold
 
-
-def train_test_plot(pred_train, y_train, pred_test, y_test, n_prev, titles, cap=4):
-    output_dim = 1 if len(y_test.shape) == 1 else y_test.shape[1]
-    if output_dim > cap:
-        output_dim = cap
-        print(output_dim)
-
-    for i in range(output_dim):
-        plt.subplot(output_dim, 2, 2 * i + 1)
-        if output_dim == 1:
-            plt.plot(pred_train, 'r', label="Predicted")
-            plt.plot(y_train[n_prev:], 'b--', label="Actual")
-        else:
-            plt.plot(pred_train[:, i], 'r', label="Predicted")
-            plt.plot(y_train[n_prev:, i], 'b--', label="Actual")
-        # nprev: because the first predicted point needed n_prev steps of data
-        # plt.title("Training performance of " + titles[i])
-        # plt.legend(loc='lower right')
-
-        plt.subplot(output_dim, 2, 2 * i + 2)
-        if output_dim == 1:
-            plt.plot(pred_test, 'r', label="Predicted")
-            plt.plot(y_test[n_prev:], 'b--', label="Actual")
-        else:
-            plt.plot(pred_test[:, i], 'r', label="Predicted")
-            plt.plot(y_test[n_prev:, i], 'b--', label="Actual")
-            # nprev: because the first predicted point needed n_prev steps of data
-            # plt.title("Testing performance of " + titles[i])
-            # plt.legend(loc='lower left')
-
-    plt.gcf().set_size_inches(15, 6)
-    plt.show()
+def mse(X1, X2, multioutput='raw_values'):
+    if multioutput == 'raw_values':
+        return np.mean((X1 - X2)**2, axis=0)**.5
+    if multioutput == 'uniform_average':
+        return np.mean(np.mean((X1 - X2)**2, axis=0)**.5)
 
 
 X = datasets('sp500')
+names = list(X.columns.values)
 X_train, X_test = time_series_split(X)
 
+for n_prev in range(1, 5):
+    tsr = TimeSeriesRegressor(LinearRegression(), n_prev=n_prev)
+    tsr.fit(X_train)
+    fc = tsr.forecast(X_train, len(X_test))
 
-n_prev = 3
-tsr = TimeSeriesRegressor(LinearRegression(), n_prev=n_prev)
-tsr.fit(X_train)
-fc = tsr.forecast(X_train, len(X_test))
+    def changes(X, start=0,end=-1):
+        return np.array([X[end, i] - X[start, i] for i in range(X.shape[1])])
 
-def changes(X):
-    return np.array([X[-1, i] - X[0, i] for i in range(X.shape[1])])
+    n_aheads = range(1,len(X_test),1)
+    X_test_changes = [changes(X_test,end=n_ahead) for n_ahead in n_aheads]
+    fc_changes = [changes(fc,end=n_ahead) for n_ahead in n_aheads]
+    mses = [mse(X_test_changes[i],fc_changes[i]) for i in range(len(n_aheads))]
+    cors = [np.corrcoef(X_test_changes[i],fc_changes[i])[0,1] for i in range(len(n_aheads))]
+    plt.plot(n_aheads,cors, label="n_prev={}".format(n_prev))
 
-#X_test_change = np.log(changes(X_test))
-#fc_change = np.log(changes(fc))
-#plt.plot(np.linspace(0,8),np.linspace(0,8),'b')
-#plt.plot(X_test_change,fc_change, 'ro')
-
-
-for i in range(min(16,X_train.shape[1])):
-    plt.subplot(4,4,i+1)
-    plt.plot(fc[:, i],'r')
-    plt.plot(X_test[:, i],'b')
-
+plt.legend()
 plt.show()
-
